6860a5752e6b86be790f7d80682f93dcd4ddb803
yingtie <yingtie@etsnet.cn>
2026-04-28 17:48:27 +0800

    chore: init project scaffold with cobra cli

---
+++ cmd/serve.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "启动 Web 面板",
+	Long:  "启动 comic-pipeline Web 管理面板",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("🎬 comic-pipeline Web 面板启动中...")
+		fmt.Println("   地址: http://localhost:8080")
+	},
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "comic-pipeline",
+	Short: "comic-pipeline CLI",
+	Long:  "comic-pipeline - 漫画处理流水线",
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+}
+
+func Execute() {
+	rootCmd.Execute()
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/kiddyt00/comic-pipeline/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
